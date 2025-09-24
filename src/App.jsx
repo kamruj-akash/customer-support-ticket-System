d50dc20b9cfc72efacc578ae295ac84856a516cc
kamruj-akash <source.akash@gmail.com>
2025-09-24 20:37:13 +0600

    Added Nav, Counter and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./Components/Footer";
+import HeroCounter from "./Components/HeroCounter";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <HeroCounter></HeroCounter>
+      <Footer></Footer>
     </>
   );
 }
